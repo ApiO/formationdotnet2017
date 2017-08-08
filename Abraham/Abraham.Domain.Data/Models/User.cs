2883d69e99e1d549d7b1b67f02bd9bac99180df6
Vincent Prince <vincent.prince.fr@gmail.com>
2017-08-08 13:29:37 +0200

    Add Repository For User, Add project structure

---
+++ Abraham/Abraham.Domain.Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Abraham.Domain.Data.Models
+{
+    public class User
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public float Balance;
+    }
+}
---
+++ Abraham/Abraham.Domain.Data/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Abraham.Domain.Data.Interfaces;
+using Abraham.Domain.Data.Models;
+using System.Linq;
+using Dapper;
+
+namespace Abraham.Domain.Data.Repositories
+{
+    public class UserRepository : SqlRepository, IUserRepository
+    {
+        public UserRepository(string connectionString) 
+            : base(connectionString)
+        {
+        }
+
+        public User Get(int id)
+        {
+            var param = new
+            {
+                id = id
+            };
+            
+            using (var ctx = GetDbContext())
+            {
+                return ctx.Connection
+                            .Query<User>("SELECT Id, FirstName, LastName, Email, Balance FROM FOO.VW_User WHERE id = {id}", param, commandType: System.Data.CommandType.Text)
+                            .First();
+            }
+        }
+    }
+}
