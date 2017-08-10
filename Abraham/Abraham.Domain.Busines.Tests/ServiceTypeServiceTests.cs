e8182935f27e6672355fd37b51e8064bcc63028e
Polochon <thepolochon@yahoo.fr>
2017-08-10 11:18:29 +0200

    Add testing

--- Abraham/Abraham.Domain.Busines.Tests/ServiceTypeServiceTests.cs
+++ Abraham/Abraham.Domain.Busines.Tests/ServiceTypeServiceTests.cs
@@ -4,6 +4,7 @@ using Abraham.Domain.Business.Services;
 using Abraham.Domain.Data.Interfaces;
 using Abraham.Domain.Data.Repositories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FakeItEasy;
 
 namespace Abraham.Domain.Busines.Tests
 {
@@ -16,8 +17,9 @@ namespace Abraham.Domain.Busines.Tests
         [TestInitialize]
         public void TestInit()
         {
-            _repository = new ServiceTypeRepository("");
-            _service = new ServiceTypeService(_repository);
+            //_repository = new ServiceTypeRepository("");
+            _repository = A.Fake<IServiceTypeRepository>();
+            
         }
 
         [TestMethod]
@@ -34,12 +36,40 @@ namespace Abraham.Domain.Busines.Tests
         }
         
         [TestMethod]
-        public void GetThrowInvalidOperationException()
+        public void ServiceTypeServiceGetAreEqual()
         {
-            Assert.ThrowsException<InvalidOperationException>(() =>
-            {
-                var serviceType = _service.Get(42);
-            });
+            Data.Models.ServiceType retourRepositiry = new Data.Models.ServiceType() { Id = 42, Description = "toto" };
+            Business.DTOs.ServiceType retourService = new Business.DTOs.ServiceType() { Id = 42, Description = "toto" };
+
+            A.CallTo(() => _repository.Get(0)).WithAnyArguments().Returns(retourRepositiry);
+            _service = new ServiceTypeService(_repository);
+
+            var serviceType = _service.Get(42);
+
+            Assert.AreEqual(retourService.Id, serviceType.Id);
+            Assert.AreEqual(retourService.Description, serviceType.Description);
+        }
+
+        [TestMethod]
+        public void ServiceTypeServiceAddAreEqual()
+        {
+            A.CallTo(() => _repository.Add(null)).WithAnyArguments().Returns(42);
+            _service = new ServiceTypeService(_repository);
+
+            var newId = _service.Add(new Business.DTOs.ServiceType());
+
+            Assert.AreEqual(42, newId);
+        }
+
+        [TestMethod]
+        public void ServiceTypeServiceDescriptionExistsIsTrue()
+        {
+            A.CallTo(() => _repository.DescriptionExists(string.Empty)).WithAnyArguments().Returns(true);
+            _service = new ServiceTypeService(_repository);
+
+            var retour = _service.DescriptionExists("toto");
+
+            Assert.AreEqual(true, retour);
         }
     }
 }
